8cb9e4d22f45717131cc6ffd8f7b4315df500ffa
Kraksaan Times <kraksaantimes@gmail.com>
2016-12-22 14:41:21 +0700

    Create recentpost.js

---
+++ recentpost.js
@@ -0,0 +1,2 @@
+document.write(&#39;&lt;div class=&quot;recent-post-title&quot;&gt;&lt;h2&gt;&lt;a href=&quot;/search/label/<data:content/>?max-results=0&quot;&gt;<data:title/>&lt;/a&gt;&lt;/h2&gt;&lt;/div&gt;&#39;);
+	  document.write(&quot;&lt;script src=\&quot;/feeds/posts/default/-/<data:content/>?orderby=updated&amp;alt=json-in-script&amp;callback=labelthumbs\&quot;&gt;&lt;\/script&gt;&quot;);
